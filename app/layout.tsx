4a7b289a82989ddd89b134687f8774ca4abb4318
eberthaud@hellowork.com <eberthaud@hellowork.com>
2025-09-15 15:06:56 +0200

    🧪: config tailwind base + add ui-library component button an story

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist } from "next/font/google";
 import "./globals.css";
+import { twMerge } from "tailwind-merge";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const geist = Geist({
   subsets: ["latin"],
 });
 
@@ -23,10 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className="h-full">
+      <body className={twMerge(geist.className, 'h-full')}>
         {children}
       </body>
     </html>
---
+++ app/login/layout.tsx
@@ -0,0 +1,6 @@
+export default () => {
+  return (
+    <div className="h-full bg-sky-500">
+    </div>
+  )
+}
