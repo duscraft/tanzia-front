88ac79f95a4775e03d45cabfbfc52d34a9e624ac
eberthaud@hellowork.com <eberthaud@hellowork.com>
2025-09-25 15:09:14 +0200

    clean

--- ui-library/components/Checkbox/Checkbox.tsx
+++ ui-library/components/Checkbox/Checkbox.tsx
@@ -6,16 +6,18 @@ import { Paragraph } from "@/ui-library/Typos/Paragraph";
 
 interface CheckboxProps {
   label?: string;
+  onChange?: () => void;
 }
 
 const Checkbox = ({
-  label = ''
+  label = '',
+  onChange
 }: CheckboxProps) => {
   return (
     <div className="flex items-center gap-2 cursor-pointer">
       <HeadlessCheckbox
         checked={true}
-        onChange={() => {}}
+        onChange={onChange}
         className="group size-6 rounded-md bg-white/10 p-1 ring-1 ring-indigo-500 ring-inset focus:not-data-focus:outline-none data-checked:bg-white data-focus:outline data-focus:outline-offset-2 data-focus:outline-white"
       >
         <CheckIcon color="#022859" className="text-indigo-500" />
